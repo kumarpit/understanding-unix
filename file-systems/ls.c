907713c42372d100340b55a15315e9734f819a7b
krarpit <kumar.arpit77@yahoo.com>
2022-11-17 13:17:57 -0800

    slightly less barebones ls

--- file-systems/ls.c
+++ file-systems/ls.c
@@ -6,17 +6,50 @@
 #include <string.h>
 #include <unistd.h>
 
+#define COLUMN_SIZE 5
+
 // barebones ls implementation
 
-int main() {
+char *out[1024];
+int idx = 0;
+int ccount = 0;
+
+void do_ls(char []);
+int pstrcmp(const void *a, const void *b);
+
+int main(int ac,  char *av[]) {
+    if (ac == 1) {
+        do_ls(".");
+    } else {
+        while (--ac) {
+            printf("%s\n", *++av);
+            do_ls(*av);
+        }
+    }
+    qsort(out, idx, sizeof(out[0]), pstrcmp);
+    for (int i=0; i < idx; i++) {
+        if (ccount == COLUMN_SIZE) {
+            printf("%s\n", out[i]);
+            ccount = 0;
+        }
+        printf("%s  ", out[i]);
+        ccount++;
+    }
+}
+
+void do_ls(char dirname[]) {
     DIR *dir_ptr;
     struct dirent *direntp;
-    dir_ptr = opendir(".");
+    dir_ptr = opendir(dirname);
     if (dir_ptr == NULL) {
-        perror(".");
+        perror("err");
         exit(1);
     }
     while ((direntp = readdir(dir_ptr)) != NULL) {
-        printf("%s\n", direntp->d_name);    
+        out[idx++] = direntp->d_name;
     }
+}
+
+int pstrcmp( const void* a, const void* b ) {
+  return strcmp( *(const char**)a, *(const char**)b );
 }
